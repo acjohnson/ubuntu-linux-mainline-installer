26c4780ad4462a2b442354fdabd743a8cfbf2335
Aaron Johnson <acjohnson@pcdomain.com>
2021-09-01 20:58:54 -0500

    minor improvements

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+#VERSION="5.10"
+#VERSION="5.10.36"
+
+VERSION="5.10.61"
+
+REPO="https://kernel.ubuntu.com/~kernel-ppa/mainline/v${VERSION}/amd64/"
+
 rm -rf amd64
-lftp -c 'mirror --parallel=100 https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/ ;exit'
-apt install $(pwd)/amd64/linux*all*.deb $(pwd)/amd64/linux*generic*.deb
+lftp -c "mirror --parallel=100 ${REPO}; exit"
+apt install $(pwd)/amd64/linux*$VERSION*all*.deb $(pwd)/amd64/linux*$VERSION*generic*.deb
 
