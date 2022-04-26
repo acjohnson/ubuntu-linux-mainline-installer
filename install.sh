2c4a4bb26e4dd3517094531d734274a0fcdea0af
Aaron Johnson <acjohnson@pcdomain.com>
2022-04-25 20:53:47 -0500

    changes

--- install.sh
+++ install.sh
@@ -3,11 +3,10 @@
 #VERSION="5.10"
 #VERSION="5.10.36"
 
-VERSION="5.10.61"
+VERSION="5.17.4"
 
 REPO="https://kernel.ubuntu.com/~kernel-ppa/mainline/v${VERSION}/amd64/"
 
 rm -rf amd64
-lftp -c "mirror --parallel=100 ${REPO}; exit"
+lftp -c "set ssl:verify-certificate no; mirror --parallel=100 ${REPO}; exit"
 apt install $(pwd)/amd64/linux*$VERSION*all*.deb $(pwd)/amd64/linux*$VERSION*generic*.deb
-
